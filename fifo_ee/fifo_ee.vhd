bc64a50432cabe750e3a4a3d38eea38bb91402a9
eneserdin <eneserdin@gmail.com>
2016-04-20 22:32:11 +0300

    some bugs corrected

--- fifo_ee/fifo_ee.vhd
+++ fifo_ee/fifo_ee.vhd
@@ -4,19 +4,24 @@ use IEEE.numeric_std.all;
 
 entity FIFO_ee is
 generic(
-   C_DATA_WIDTH : integer := 10
-  ;C_DEPTH      : integer := 10
+   C_DATA_WIDTH     : integer := 10
+  ;C_DEPTH          : integer := 10
   );
 port(
-   clk          : in std_logic
-  ;rst          : in std_logic
-  ;datain       : in std_logic_vector(C_DATA_WIDTH-1 downto 0)
-  ;dataout      : out std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0')
-  ;wr_en        : in std_logic
-  ;rd_en        : in std_logic
-  ;full         : out std_logic
-  ;empty        : out std_logic
-  ;data_count   : out std_logic_vector(C_DEPTH - 1 downto 0)
+   clk              : in std_logic
+  ;din              : in std_logic_vector(C_DATA_WIDTH-1 downto 0)
+  ;prog_full_thresh : in std_logic_vector(C_DEPTH-1 downto 0)
+  ;rd_en            : in std_logic
+  ;srst             : in std_logic
+  ;wr_en            : in std_logic
+  ;dout             : out std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0')
+  ;empty            : out std_logic
+  ;full             : out std_logic
+  ;overflow         : out std_logic
+  ;prog_full        : out std_logic
+  ;valid            : out std_logic
+  ;wr_ack           : out std_logic
+  ;data_count       : out std_logic_vector(C_DEPTH - 1 downto 0)
   );
 end entity;
 
@@ -28,6 +33,7 @@ architecture behavioral of FIFO_ee is
 
   signal empty_i : std_logic;
   signal full_i : std_logic;
+  signal prog_full_i : std_logic;
 
 
   signal wr_ptr : unsigned(C_DEPTH downto 0) := (others => '0');
@@ -38,15 +44,23 @@ architecture behavioral of FIFO_ee is
     process(clk)
       begin
         if rising_edge(clk) then
-          if wr_en = '1' and full_i = '0' then
-            mem(to_integer(wr_ptr(C_DEPTH-1 downto 0))) := datain;
-            wr_ptr <= wr_ptr + 1;
-          end if;
+            if srst = '1' then
+                wr_ptr <= (others => '0');
+                rd_ptr <= (others => '0');
+                wr_ack <= '0';
+            else
+                wr_ack <= '0';
+                if wr_en = '1' and full_i = '0' then
+                    mem(to_integer(wr_ptr(C_DEPTH-1 downto 0))) := din;
+                    wr_ptr <= wr_ptr + 1;
+                    wr_ack <= '1';
+                end if;
 
-          if rd_en = '1' and empty_i = '0' then
-            dataout <= mem(to_integer(rd_ptr(C_DEPTH-1 downto 0)));
-            rd_ptr <= rd_ptr + 1;
-          end if;
+                if rd_en = '1' and empty_i = '0' then
+                    dout <= mem(to_integer(rd_ptr(C_DEPTH-1 downto 0)));
+                    rd_ptr <= rd_ptr + 1;
+                end if;
+            end if;
          end if;
       end process;
 
@@ -61,22 +75,44 @@ architecture behavioral of FIFO_ee is
     variable data_count_var : unsigned(data_count'range);
     begin
         if rising_edge(clk) then
-            if rst = '1' then
+            if srst = '1' then
                 data_count_var := (others => '0');
                 data_count <= (others => '0');
+                overflow <= '0';
+                valid <= '0';
             else
+                
                 if wr_en = '1' and full_i = '0' then
                     data_count_var := data_count_var + 1;
                 end if;
                 
                 if rd_en = '1' and empty_i = '0' then
                     data_count_var := data_count_var - 1;
+                    valid <= '1';
+                else
+                    if rd_en = '1' and empty_i = '1' then
+                        valid <= '0';
+                    end if;
                 end if;
-
+                
+                if wr_en = '1' and full_i = '1' then
+                    overflow <= '1';
+                else
+                    overflow <= '0';
+                end if;
+                
+                programmable_full_threshold_gen : if unsigned(prog_full_thresh) < data_count_var then
+                    prog_full_i <= '1';
+                else
+                    prog_full_i <= '0';
+                end if;
             end if;
             data_count <= std_logic_vector(data_count_var);
         end if;
     end process;
+    
+    
+prog_full <= prog_full_i or full_i; 
                     
                 
  end Behavioral;
--- fwft_fifo_ee/fwft_fifo_ee.vhd
+++ fwft_fifo_ee/fwft_fifo_ee.vhd
@@ -8,37 +8,38 @@ generic(
   ;C_DEPTH      : integer := 6
   );
 port(
-   clk          : in std_logic
-  ;rst          : in std_logic
-  ;datain       : in std_logic_vector(C_DATA_WIDTH-1 downto 0)
-  ;dataout      : out std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0')
-  ;wr_en        : in std_logic
-  ;rd_en        : in std_logic
-  ;full         : out std_logic
-  ;empty        : out std_logic
-  ;data_count   : out std_logic_vector(C_DEPTH downto 0)
+   clk              : in std_logic
+  ;din              : in std_logic_vector(C_DATA_WIDTH-1 downto 0)
+  ;prog_full_thresh : in std_logic_vector(C_DEPTH-1 downto 0)
+  ;rd_en            : in std_logic
+  ;srst             : in std_logic
+  ;wr_en            : in std_logic
+  ;dout             : out std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0')
+  ;full             : out std_logic
+  ;valid            : out std_logic
+  ;overflow         : out std_logic
+  ;prog_full        : out std_logic
+  ;wr_ack           : out std_logic
+  ;empty            : out std_logic
+  ;data_count       : out std_logic_vector(C_DEPTH downto 0)
   );
 end entity;
 
 Architecture behav of fwft_fifo_ee is
 
---signal clk          : std_logic;
---signal rst          : std_logic;
---signal datain       : std_logic_vector(C_DATA_WIDTH-1 downto 0);
-signal dataout_fifo   : std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0');
-signal dataout_r      : std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0');
---signal wr_en        : std_logic;
-signal rd_en_fifo        : std_logic;
-signal rd_en_fifo_r        : std_logic;
-signal full_fifo         : std_logic;
-signal empty_fifo        : std_logic;
-signal empty_fifo_r        : std_logic;
-signal data_count_fifo   : std_logic_vector(C_DEPTH - 1 downto 0);
+signal dout_fifo        : std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0');
+signal dout_r           : std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others => '0');
+signal rd_en_fifo       : std_logic;
+signal rd_en_fifo_r     : std_logic;
+signal full_fifo        : std_logic;
+signal empty_fifo       : std_logic;
+signal empty_fifo_r     : std_logic;
+signal data_count_fifo  : std_logic_vector(C_DEPTH - 1 downto 0);
 
-signal state : integer := 0;
-signal rd_en_fsm : std_logic := '0';
+signal state        : integer := 0;
+signal rd_en_fsm    : std_logic := '0';
 
-signal empty_sig : std_logic := '1';
+signal empty_sig    : std_logic := '1';
 
 begin
 
@@ -49,15 +50,20 @@ generic map(
     ,C_DEPTH      => C_DEPTH
   )
 port map(
-     clk          => clk       
-    ,rst          => rst       
-    ,datain       => datain    
-    ,dataout      => dataout_fifo
-    ,wr_en        => wr_en     
-    ,rd_en        => rd_en_fifo
-    ,full         => full_fifo     
-    ,empty        => empty_fifo 
-    ,data_count   => data_count_fifo
+     clk                => clk       
+    ,din                => din    
+    ,prog_full_thresh   => prog_full_thresh
+    ,rd_en              => rd_en_fifo
+    ,srst               => srst       
+    ,wr_en              => wr_en     
+    ,dout               => dout_fifo
+    ,empty              => empty_fifo 
+    ,full               => full_fifo     
+    ,overflow           => overflow
+    ,prog_full          => prog_full
+    ,valid              => valid
+    ,wr_ack             => wr_ack
+    ,data_count         => data_count_fifo
 );
 
 
@@ -67,7 +73,7 @@ rd_en_fifo <= '1' when rd_en = '1' or rd_en_fsm = '1' or ((empty_fifo = '0' and 
 process(clk)
 begin
     if rising_edge(clk) then
-        if rst = '1' then
+        if srst = '1' then
             empty_fifo_r <= '0';
             rd_en_fsm <= '0';
             state <= 0;
@@ -95,7 +101,7 @@ begin
             end loop;
             
             if rd_en_fifo = '1' and state /= 0 then
-                dataout_r <= dataout_fifo;
+                dout_r <= dout_fifo;
             end if;
             
 
@@ -104,7 +110,7 @@ begin
     end if;
 end process;
 
-dataout <= dataout_fifo;-- when rd_en_fifo_r = '1';
+dout <= dout_fifo;
 data_count <= std_logic_vector(to_unsigned(state,data_count'length));
 empty_sig <= '1' when state = 0 else '0';
 empty <= empty_sig;
